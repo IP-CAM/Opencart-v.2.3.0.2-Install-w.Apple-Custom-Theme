d4ba933f84cab35dc9d82aa005949f84173f4b3d
Serhiy Kotsyuba <s-p-ko@ukr.net>
2019-03-18 11:18:23 +0200

    1st commit

---
+++ config_.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/OpenServer/domains/opencart.loc/catalog/');
+define('DIR_SYSTEM', 'F:/OpenServer/domains/opencart.loc/system/');
+define('DIR_IMAGE', 'F:/OpenServer/domains/opencart.loc/image/');
+define('DIR_LANGUAGE', 'F:/OpenServer/domains/opencart.loc/catalog/language/');
+define('DIR_TEMPLATE', 'F:/OpenServer/domains/opencart.loc/catalog/view/theme/');
+define('DIR_CONFIG', 'F:/OpenServer/domains/opencart.loc/system/config/');
+define('DIR_CACHE', 'F:/OpenServer/domains/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/OpenServer/domains/opencart.loc/system/storage/download/');
+define('DIR_LOGS', 'F:/OpenServer/domains/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/OpenServer/domains/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/OpenServer/domains/opencart.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
